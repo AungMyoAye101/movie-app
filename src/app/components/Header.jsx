48c01bd54a77c10f7cbfa83bcc66128b3db2faac
AungMyoAye <aungmyoaye101@gmail.com>
2024-07-01 13:18:08 +0630

    added react icon

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-
+import MenuItems from "./MenuItems";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
 export default function Header() {
   return (
     <div className="flex justify-between py-2 px-6 text-lg bg-violet-500">
       <div className="flex gap-4 ">
-        <div>Home</div>
-        <div>About</div>
+        <MenuItems title="home" address="/" Icon={AiFillHome} />
+        <MenuItems title="About" address="/" Icon={BsFillInfoCircleFill} />
       </div>
       <div className="flex gap-4">
-        <div>Dark</div>
-        <div>Logo</div>
+        <MenuItems title="Dark" address="/" Icon={AiFillHome} />
+        <MenuItems title="Logo" address="/" Icon={BsFillInfoCircleFill} />
       </div>
     </div>
   );
--- src/app/components/MenuItems.jsx
+++ src/app/components/MenuItems.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Link from "next/link";
 
-export default function MenuItems() {
-  return <div>MenuItems</div>;
+export default function MenuItems({ title, address, Icon }) {
+  return (
+    <Link href={address} className="hover:text-amber-400 hover:underline">
+      <Icon className="text-2xl sm:hidden" />
+      <p className="uppercase text-lg hidden sm:inline">{title}</p>
+    </Link>
+  );
 }
