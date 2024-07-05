98dc44943ec2de3bad8dd522e3af2eda41283d90
AungMyoAye <aungmyoaye101@gmail.com>
2024-07-05 12:28:52 +0630

    add api key

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <div>Hello Apiral</div>;
+"use client";
+import { useEffect } from "react";
+
+const API_KEY = process.env.API_KEY;
+export default function Home({ searchParams }) {
+  useEffect(() => {
+    const genre = searchParams.genre || "fetchTrending";
+    const res = fetch(
+      `https://www.themoviedb.org/3${
+        genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week"
+      }?api_key=${API_KEY}&language=en-US&pg=1`
+    );
+    console.log(res.json());
+  }, []);
+
+  // if (!res.ok) {
+  //   console.log("erroor");
+  // }
+  // const result = data.results;
+  // console.log(result);
+  return <div>dd</div>;
 }
