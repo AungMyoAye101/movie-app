86528e2f9e23ef1f07a75250d925bcfa096cb18a
AungMyoAye <aungmyoaye101@gmail.com>
2024-07-05 21:33:44 +0630

    Fix:fetching data

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
+const API_KEY = process.env.API_KEY;
 import React from "react";
 
 const Home = async () => {
-  const res = await fetch("https://dummyjson.com/products");
-  const users = await res.json();
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`
+  );
+  const result = await res.json();
+
   return (
     <div>
       <ul>
-        {users.products.map((item) => (
+        {result.results.map((item) => (
           <li key={item.id}>{item.title}</li>
         ))}
       </ul>
