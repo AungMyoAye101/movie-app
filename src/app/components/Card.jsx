f6d1a0d9aa702e3c49bd13174a6fe06809ff53a2
AungMyoAye <aungmyoaye101@gmail.com>
2024-07-06 11:08:48 +0630

    loaded images file sucessfully

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -1,15 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 const Card = ({ title, poster, overview, releaseDate, count }) => {
   return (
-    <div className=" rounded p-2">
-      <img
-        className="block w-full mb-2"
-        src={`https://media.themoviedb.org/t/p${poster}`}
-        alt="poster"
+    <div className=" rounded max-w-72 mx-auto ">
+      <Image
+        src={`https://image.tmdb.org/t/p/original/${poster}`}
+        width={400}
+        height={200}
+        alt="posters"
+        className="rounded-t"
       />
-      <div className="flex flex-col gap-2">
-        <h1>{title}</h1>
+      <div className="flex flex-col gap-2 p-2">
+        <h1 className="transcate">{title}</h1>
         <div className="max-h-16 overflow-hidden text-sm">{overview}</div>
         <div className="flex justify-between">
           <span>{releaseDate}</span>
--- src/app/page.js
+++ src/app/page.js
@@ -10,11 +10,12 @@ const Home = async () => {
 
   return (
     <div>
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {result.results.map((item) => (
           <Card
             key={item.id}
             title={item.title}
+            poster={item.poster_path}
             overview={item.overview}
             count={item.vote_count}
             releaseDate={item.release_date}
