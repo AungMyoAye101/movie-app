f44ef1b01ec37961c081dd995c2202be50ebac54
AungMyoAye <aungmyoaye101@gmail.com>
2024-07-12 19:53:57 +0630

    Fix: search page

--- src/app/search/[searchText]/page.jsx
+++ src/app/search/[searchText]/page.jsx
@@ -8,13 +8,15 @@ const SearchPage = async ({ params }) => {
   );
   const data = await res.json();
   const results = data.results;
-  console.log(results);
-  return;
-  <div>
-    {results &&
-      results.length === <h1 className="text-center pt-6">No results found</h1>}
-    {results && <Result results={results} />}
-  </div>;
+
+  return (
+    <div>
+      {results &&
+        results.length ===
+        <h1 className="text-center pt-6">No results found</h1>}
+      {results && <Result results={results} />}
+    </div>
+  );
 };
 
 export default SearchPage;
