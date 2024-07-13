e682f9bb6ae78007f4d6182a4a5987938146f532
AungMyoAye <aungmyoaye101@gmail.com>
2024-07-13 06:37:11 +0630

    changed card padding

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Card = ({ id, title, poster, overview, releaseDate, count }) => {
   return (
-    <div className=" card  bg-slate-100 dark:bg-gray-800 shadow-xl ">
+    <div className=" card  ">
       <Link href={`/movie/${id}`}>
         <Image
           src={`https://image.tmdb.org/t/p/original/${poster}`}
@@ -15,9 +15,9 @@ const Card = ({ id, title, poster, overview, releaseDate, count }) => {
           style={{ maxWidth: "100%", aspectRatio: "1/1", objectFit: "fill" }}
         />
       </Link>
-      <div className="card-body">
+      <div className="card-body px-2">
         <h2 className="card-title truncate">{title}</h2>
-        <div className="line-clamp-3 overflow-hidden text-sm">{overview}</div>
+        <div className="line-clamp-2 overflow-hidden text-sm">{overview}</div>
         <div className="flex justify-between">
           <span>{releaseDate}</span>
           <span>{count}</span>
